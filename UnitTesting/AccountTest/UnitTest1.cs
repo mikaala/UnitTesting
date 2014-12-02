1d594ecc7a7a551efafe1f457d55687d300e6a9e
mikaala <mikaal@metropolia.fi>
2014-12-02 23:16:12 +0200

    Push first version

---
+++ UnitTesting/AccountTest/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace AccountTest
+{
+    [TestClass]
+    public class Tester
+    {
+        [TestMethod]
+        public void testConstructorNegativeAmount()
+        {
+            BankAccount account = new BankAccount("Matti", -1);
+            Assert.AreEqual(account.Balance, -1);
+            //
+        }
+        [TestMethod]
+        public void testConstructorPositiveAmount()
+        {
+            BankAccount account = new BankAccount("Matti", 1);
+            Assert.AreEqual(account.Balance, 1);
+        }
+        [TestMethod]
+        public void testConstructorPositiveDouble()
+        {
+            BankAccount account = new BankAccount("Matti", 1.11);
+            Assert.AreEqual(account.Balance, 1.11);
+        }
+        [TestMethod]
+        public void testConstructorTooHighAmount()
+        {
+            BankAccount account = new BankAccount("Matti", Double.MaxValue + 1000);
+            //Not higher than max double
+            Assert.AreEqual(account.Balance, Double.MaxValue);
+        }
+        [TestMethod]
+        public void testDebitTooMuch()
+        {
+            BankAccount account = new BankAccount("Matti", 500);
+            account.Debit(501);
+            //Assert that it did not change the balance
+            Assert.AreEqual(account.Balance, 500);
+        }
+        [TestMethod]
+        public void testDebitTooLittle()
+        {
+            BankAccount account = new BankAccount("Matti", 500);
+            account.Debit(-1);
+            //Assert that it did not change the balance
+            Assert.AreEqual(account.Balance, 500);
+        }
+        [TestMethod]
+        public void testDebitNormal()
+        {
+            BankAccount account = new BankAccount("Matti", 500);
+            account.Debit(250);
+            Assert.AreEqual(account.Balance, 250);
+        }
+        [TestMethod]
+        public void testCreditOverBalance()
+        {
+            BankAccount account = new BankAccount("Matti", 500);
+            account.Credit(501);
+            //Assert that it did become negative
+            Assert.AreEqual(account.Balance, 500 - 501);
+        }
+        [TestMethod]
+        public void testCreditTooMuch()
+        {
+            BankAccount account = new BankAccount("Matti", 500);
+            account.Credit(6000);
+            //Assert that the account froze
+            Assert.AreEqual(account.Frozen, true);
+        }
+        [TestMethod]
+        public void testCreditTooLittle()
+        {
+            BankAccount account = new BankAccount("Matti", 500);
+            account.Credit(-1);
+            //Assert that it did not change the balance
+            Assert.AreEqual(account.Balance, 500);
+        }
+        [TestMethod]
+        public void testDepositNormal()
+        {
+            BankAccount account = new BankAccount("Matti", 500);
+            account.Deposit(500);
+            Assert.AreEqual(account.Balance, 500 + 500);
+        }
+        [TestMethod]
+        public void testDepositTooLittle()
+        {
+            BankAccount account = new BankAccount("Matti", 500);
+            account.Deposit(-1);
+            Assert.AreEqual(account.Balance, 500);
+        }
+    }
+}
+
---
+++ UnitTesting/UnitTesting/BankAccountNS.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace BankAccountNS
+{
+    public class BankAccount
+    {
+        private string customerName;
+        private double balance;
+        private bool accountFrozen = false;
+        private const int negativeLimit = -5000;
+
+        private BankAccount() {
+        }
+
+        public BankAccount(string customerName, double balance) {
+            this.customerName = customerName;
+            this.balance = balance;
+        }
+
+        public string CustomerName {
+            get { return customerName; }
+        }
+
+        public double Balance {
+            get { return balance; }
+        }
+
+        public bool Frozen
+        {
+            get { return accountFrozen; }
+        }
+
+        public void Deposit(double amount) {
+            if (!accountFrozen && amount > 0) {
+                balance += amount;
+            }
+        }
+
+        public void Debit(double amount) {
+            if (!accountFrozen && amount < balance && amount > 0) {
+                balance -= amount;
+            }
+        }
+
+        public void Credit(double amount) {
+            if ((balance - amount) < negativeLimit && !accountFrozen) {
+                accountFrozen = true;
+                balance -= amount;
+            }
+            if (!accountFrozen && amount > 0) {
+                balance -= amount;
+            }
+        }
+
+        public static void Main()
+        {
+            BankAccount account = new BankAccount("Heikki", 53.10);
+            Console.WriteLine("Balance is ${0}", account.Balance);
+        }
+    }
+}
